f901694f44f67cf38bb36975eaca730a54702f2d
+ <+>
2023-01-15 11:08:15 +0300

    Bonus

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,8 +1,26 @@
+import React from "react";
+//Bonus Deliverables
+function minutesRead(minutes) {
+    if (minutes > 0 && minutes < 30) {
+      const times = Math.ceil(minutes / 5);
+      return "☕️".repeat(times);
+    } else if (minutes >= 30) {
+      const times = Math.ceil(minutes / 10);
+      return "🍱".repeat(times);
+    } else {
+      return "";
+    }
+}
+
+
 function Article({title, date = "January 1, 1970", preview}){
     return (
       <article>
         <h3>{title}</h3>
-        <small>{date}</small>
+        <small>
+            {date} {minutesRead(minutes)}
+            {minutes} min read
+        </small>
         <p>{preview}</p>
       </article>
     )
